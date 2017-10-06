580351bab44a3f2e7cff168f98e06d6de7f3eb36
Sergiy Tertychnij <sergiy@qubyx.com>
2017-10-06 09:49:13 +0300

    add base routing example

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorksComponent } from './works/works.component';
+
+import {Routes, RouterModule} from "@angular/router";
+
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'works', component: WorksComponent},
+  {path: '**', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
