0cef8b2e7a4f2d60895ea7495392e8620ac57f38
Sergiy Tertychnij <sergiy@qubyx.com>
2017-11-09 16:58:58 +0200

    theme update

--- client/src/app/theme/theme.component.ts
+++ client/src/app/theme/theme.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { ThemeService } from './theme.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-theme',
@@ -8,9 +10,15 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class ThemeComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private authService: AuthService,
+              private themeService: ThemeService) {
     this.route.params.subscribe(params => {
       console.log('params - ', params);
+      themeService.get(params.theme)
+      .then(result=>{
+          console.log('theme result - ', result.json())
+      })
     })
   }
 
